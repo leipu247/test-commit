3c71a8699450fb6d3dda9e77c39a381fe85e0539
leipu <200684632@qq.com>
2024-12-18 14:57:25 +0800

    message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es2021": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:vue/essential"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 12
-  },
-  "plugins": [
-    "vue"
-  ],
-  "rules": {
-  },
-  "ignorePatterns": ["*.json", "**/vendor/*.js"],
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
 };
