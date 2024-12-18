958209d60e9365501ad142b257d7808ff4d7d463
leipu <200684632@qq.com>
2024-12-18 19:08:31 +0800

    fix: 222

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,27 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  prompt: {
+    questions: {
+      type: {
+        description: '选择你要提交的类型:',
+        enum: {
+          feat: {
+            description: '新功能',
+            title: 'Features',
+            emoji: '✨'
+          },
+          fix: {
+            description: '修复相关bug',
+            title: 'Bug Fixes',
+            emoji: '🐛'
+          },
+          docs: {
+            description: '文档更改',
+            title: 'Documentation',
+            emoji: '📚'
+          }
+        }
+      }
+    }
+  }
+}
